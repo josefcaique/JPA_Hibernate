797fe77dff86602d7079eb6a4247b4984d969c94
Josef Caique <jcaique10@gmail.com>
2025-02-22 23:52:25 -0300

    adding User class

---
+++ src/main/java/JPA/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package JPA.course.resources;
+
+import JPA.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Josef", "josef@email.com", "11954845421", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
