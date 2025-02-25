024303fdd0132940f829f12befc9b39777f4038a
Josef Caique <jcaique10@gmail.com>
2025-02-24 23:05:59 -0300

    adding category class

--- src/main/java/JPA/course/config/TestConfig.java
+++ src/main/java/JPA/course/config/TestConfig.java
@@ -1,9 +1,11 @@
 package JPA.course.config;
 
 
+import JPA.course.entities.Category;
 import JPA.course.entities.Order;
 import JPA.course.entities.User;
 import JPA.course.entities.enuns.OrderStatus;
+import JPA.course.repositories.CategoryRepository;
 import JPA.course.repositories.OrderRepository;
 import JPA.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -35,5 +40,11 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
     }
 }
