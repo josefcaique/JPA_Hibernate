0270c65d290103e965210e277c953e54ec31eb55
Josef Caique <jcaique10@gmail.com>
2025-02-24 22:19:18 -0300

    order status enum

--- src/main/java/JPA/course/entities/Order.java
+++ src/main/java/JPA/course/entities/Order.java
@@ -1,5 +1,6 @@
 package JPA.course.entities;
 
+import JPA.course.entities.enuns.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -23,6 +24,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -30,9 +32,10 @@ public class Order implements Serializable {
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -59,6 +62,14 @@ public class Order implements Serializable {
 
     public void setCliet(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) this.orderStatus = orderStatus.getCode();
     }
 
     @Override
