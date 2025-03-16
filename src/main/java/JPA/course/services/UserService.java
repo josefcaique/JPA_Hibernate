8997567e49bdb55b8621273f801a1655eb32d8c5
Josef Caique <jcaique10@gmail.com>
2025-03-16 19:06:17 -0300

    Adding exceptions

--- src/main/java/JPA/course/services/UserService.java
+++ src/main/java/JPA/course/services/UserService.java
@@ -2,6 +2,7 @@ package JPA.course.services;
 
 import JPA.course.entities.User;
 import JPA.course.repositories.UserRepository;
+import JPA.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
