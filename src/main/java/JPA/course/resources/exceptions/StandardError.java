ddddc4a46adafef4f8caf674a73e7e717be05848
Josef Caique <jcaique10@gmail.com>
2025-03-16 22:12:05 -0300

    Exception handling delete

--- src/main/java/JPA/course/resources/exceptions/StandardError.java
+++ src/main/java/JPA/course/resources/exceptions/StandardError.java
@@ -1,5 +1,7 @@
 package JPA.course.resources.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -21,6 +23,7 @@ public class StandardError implements Serializable {
         this.path = path;
     }
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     public Instant getTimestamp() {
         return timestamp;
     }
---
+++ src/main/java/JPA/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package JPA.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+
+}
